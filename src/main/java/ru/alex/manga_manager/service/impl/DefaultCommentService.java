34cf296e18bea3ab3ddcb2afd82516c638f77b23
scorp <3048453@gmail.com>
2024-01-17 18:30:42 +0300

    add settings

--- src/main/java/ru/alex/manga_manager/service/impl/DefaultCommentService.java
+++ src/main/java/ru/alex/manga_manager/service/impl/DefaultCommentService.java
@@ -45,6 +45,10 @@ public class DefaultCommentService implements CommentService {
     public boolean add(RegistrationNewCommentDto commentDto) {
         Comment comment = new Comment();
 
+        if (commentDto.getAuthentication() == null) {
+            throw new ForbiddenException("Forbidden");
+        }
+
         String id = commentDto.getAuthentication().getName();
         User user = userRepository.findById(id).orElseThrow(() ->
                 new UserNotFoundException("User: " + id + " Not Found"));
--- src/main/java/ru/alex/manga_manager/util/exception/ForbiddenException.java
+++ src/main/java/ru/alex/manga_manager/util/exception/ForbiddenException.java
@@ -1,4 +1,7 @@
 package ru.alex.manga_manager.util.exception;
 
 public class ForbiddenException extends RuntimeException {
+    public ForbiddenException(String message) {
+        super(message);
+    }
 }
