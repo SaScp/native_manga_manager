4ef09b31225f3f410dc95e46c579dcebffae1d32
scorp <3048453@gmail.com>
2024-01-22 00:08:05 +0300

    add

--- src/main/java/ru/alex/manga_manager/config/SecurityConfiguration.java
+++ src/main/java/ru/alex/manga_manager/config/SecurityConfiguration.java
@@ -94,7 +94,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource configurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(List.of("*"));
