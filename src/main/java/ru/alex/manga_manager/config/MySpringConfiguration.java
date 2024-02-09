26337dafeca987ac3d59a2accab1879de05198d7
Alexander Vorobev <3048453@gmail.com>
2024-02-09 22:13:57 +0300

    add lib

--- src/main/java/ru/alex/manga_manager/config/MySpringConfiguration.java
+++ src/main/java/ru/alex/manga_manager/config/MySpringConfiguration.java
@@ -1,17 +1,19 @@
 package ru.alex.manga_manager.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.yandexparser.baseLogic.Cloud;
 
 import java.util.List;
 
 @Configuration
 @EnableWebMvc
-public class MvcConfiguration implements WebMvcConfigurer {
+public class MySpringConfiguration implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         WebMvcConfigurer.super.addArgumentResolvers(resolvers);
@@ -22,5 +24,13 @@ public class MvcConfiguration implements WebMvcConfigurer {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Value("${yandex.disk.token}")
+    private String yandexToken;
+
+    @Bean
+    public Cloud cloud() {
+        return Cloud.builder().token(yandexToken).build();
     }
 }
